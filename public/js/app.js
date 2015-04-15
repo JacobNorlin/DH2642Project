9f98b80278610e4c10c247b8a0bc953e90d1f23b
mangefox <fox@fojji.se>
2015-04-15 02:29:09 +0200

    Style tweaks

--- public/js/app.js
+++ public/js/app.js
@@ -3,3 +3,7 @@
 
 // Declare app level module which depends on filters, and services
 var app = angular.module('myApp', ['myApp.filters', 'myApp.directives', 'xeditable']);
+
+app.run(function(editableOptions) {
+  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+});
