e5d533248c69a41d6e3f889a3ea25ef1765c5eb5
mangefox <fox@fojji.se>
2015-04-15 10:59:17 +0200

    A bunch of clean-up and formatting

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,8 @@
 'use strict';
 
-
 // Declare app level module which depends on filters, and services
-var app = angular.module('myApp', ['myApp.filters', 'myApp.directives', 'xeditable']);
+var app = angular.module('myApp', ['xeditable']);
 
 app.run(function(editableOptions) {
-  editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+	editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
 });
--- public/js/controllers.js
+++ public/js/controllers.js
@@ -4,112 +4,112 @@
 
 app.controller('AppCtrl', function ($scope, socket) {
 
-  var chat = $("#chat");
+	var chat = $("#chat");
 
-  // Socket listeners
-  // ================
+	// Socket listeners
+	// ================
 
-  socket.on('init', function (data) {
-    $scope.name = data.name;
-    $scope.users = data.users;
-  });
+	socket.on('init', function (data) {
+		$scope.name = data.name;
+		$scope.users = data.users;
+	});
 
-  socket.on('send:message', function (message) {
-    $scope.messages.push(message);
-    chat.scrollTop(chat.prop("scrollHeight")); // scroll to bottom
-    // chat.scrollTop(1e+10); // scroll to bottom
-  });
+	socket.on('send:message', function (message) {
+		$scope.messages.push(message);
+		chat.scrollTop(chat.prop("scrollHeight")); // scroll to bottom
+		// chat.scrollTop(1e+10); // scroll to bottom
+	});
 
-  socket.on('change:name', function (data) {
-    changeName(data.oldName, data.newName);
-  });
+	socket.on('change:name', function (data) {
+		changeName(data.oldName, data.newName);
+	});
 
-  socket.on('user:join', function (data) {
-    $scope.messages.push({
-      user: 'chatroom',
-      text: 'User ' + data.name + ' has joined.'
-    });
-    $scope.users.push(data.name);
-  });
+	socket.on('user:join', function (data) {
+		$scope.messages.push({
+			user: 'chatroom',
+			text: 'User ' + data.name + ' has joined.'
+		});
+		$scope.users.push(data.name);
+	});
 
-  // add a message to the conversation when a user disconnects or leaves the room
-  socket.on('user:left', function (data) {
-    $scope.messages.push({
-      user: 'chatroom',
-      text: 'User ' + data.name + ' has left.'
-    });
-    var i, user;
-    for (i = 0; i < $scope.users.length; i++) {
-      user = $scope.users[i];
-      if (user === data.name) {
-        $scope.users.splice(i, 1);
-        break;
-      }
-    }
-  });
+	// add a message to the conversation when a user disconnects or leaves the room
+	socket.on('user:left', function (data) {
+		$scope.messages.push({
+			user: 'chatroom',
+			text: 'User ' + data.name + ' has left.'
+		});
+		var i, user;
+		for (i = 0; i < $scope.users.length; i++) {
+			user = $scope.users[i];
+			if (user === data.name) {
+				$scope.users.splice(i, 1);
+				break;
+			}
+		}
+	});
 
-  // Private helpers
-  // ===============
+	// Private helpers
+	// ===============
 
-  var changeName = function (oldName, newName) {
-    // rename user in list of users
-    var i;
-    for (i = 0; i < $scope.users.length; i++) {
-      if ($scope.users[i] === oldName) {
-        $scope.users[i] = newName;
-      }
-    }
+	var changeName = function (oldName, newName) {
+		// rename user in list of users
+		var i;
+		for (i = 0; i < $scope.users.length; i++) {
+			if ($scope.users[i] === oldName) {
+				$scope.users[i] = newName;
+			}
+		}
 
-    $scope.messages.push({
-      user: 'chatroom',
-      text: 'User ' + oldName + ' is now known as ' + newName + '.'
-    });
-  }
+		$scope.messages.push({
+			user: 'chatroom',
+			text: 'User ' + oldName + ' is now known as ' + newName + '.'
+		});
+	}
 
-  // Methods published to the scope
-  // ==============================
+	// Methods published to the scope
+	// ==============================
 
-  $scope.changeName = function ($data) {
-    $scope.oldName = $scope.name;
-    socket.emit('change:name', {
-      name: $data
-    }, function (result) {
-      if (!result) {
-        $scope.name = $scope.oldName;
-        alert('There was an error changing your name');
-        return "Error changing name.";
-      } else {
+	$scope.changeName = function ($data) {
+		$scope.oldName = $scope.name;
+		socket.emit('change:name', {
+			name: $data
+		}, function (result) {
+			if (!result) {
+				$scope.name = $scope.oldName;
+				alert('There was an error changing your name');
+				return "Error changing name.";
+			} else {
 
-        changeName($scope.oldName, $data);
-        $scope.name = $data;
-      }
-    });
-  };
+				changeName($scope.oldName, $data);
+				$scope.name = $data;
+			}
+		});
+	};
 
-  $scope.messages = [];
+	$scope.messages = [];
 
-  $scope.sendMessage = function () {
-    if ($scope.message.length == 0)
-      return;
+	$scope.sendMessage = function () {
+		if ($scope.message.length == 0)
+			return;
 
-    socket.emit('send:message', {
-      message: $scope.message
-    });
+		socket.emit('send:message', {
+			message: $scope.message
+		});
 
-    // add the message to our model locally
-    $scope.messages.push({
-      user: $scope.name,
-      text: $scope.message
-    });
+		// add the message to our model locally
+		$scope.messages.push({
+			user: $scope.name,
+			text: $scope.message
+		});
 
-    // clear message box
-    $scope.message = '';
-    chat.scrollTop(chat.prop("scrollHeight")); // scroll to bottom
-    // chat.scrollTop(1e+10); // scroll to bottom
-  };
+		// clear message box
+		$scope.message = '';
+		chat.scrollTop(chat.prop("scrollHeight")); // scroll to bottom
+		// chat.scrollTop(1e+10); // scroll to bottom
+	};
 
-  $scope.togglePopup = function() {
-    $(".overlay, .popup").fadeToggle(100);
-  };
+	$scope.togglePopup = function() {
+		$(".overlay, .popup").fadeToggle(100);
+	};
 
 });
