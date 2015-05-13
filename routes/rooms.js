aa63d465c553c164a084e1835e05a7b0b910170d
jacob <jacob.norlinandersson@gmail.com>
2015-05-13 15:22:27 +0200

    Ran some kind of js formatter

--- routes/rooms.js
+++ routes/rooms.js
@@ -12,7 +12,7 @@ var request = require('request');
 var _und = require('underscore')
 
 
-var rooms = {}	
+var rooms = {}
 
 exports.MAX_USERNAME_LENGTH = 30;
 exports.MAX_MESSAGE_LENGTH = 1000;
@@ -20,18 +20,18 @@ exports.MAX_MESSAGE_LENGTH = 1000;
 /**
  * Creates a new Room object and stores it in the rooms object
  * @param roomId The id of the room*/
-exports.createNewRoom = function (roomId){
-	rooms[roomId] = new Room();
-}
-/**
- * Checks if the room is created
- * @param roomId The id of the room
- * @returns {boolean} True if the room exists, otherwise false
-*/
+exports.createNewRoom = function(roomId) {
+		rooms[roomId] = new Room();
+	}
+	/**
+	 * Checks if the room is created
+	 * @param roomId The id of the room
+	 * @returns {boolean} True if the room exists, otherwise false
+	 */
 exports.roomExists = function(roomId) {
-	for(var room in rooms){
-		console.log(room == roomId )
-		if(room == roomId){
+	for (var room in rooms) {
+		console.log(room == roomId)
+		if (room == roomId) {
 			return true;
 		}
 	}
@@ -42,26 +42,32 @@ exports.roomExists = function(roomId) {
 exports.getNumberOfPlayersOfGame = function(gameid, room) {
 	var userData = room.getUserData();
 	var numPlayers = 0;
-	for(var user in userData){
-		if(userData[user].games[gameid]){
+	for (var user in userData) {
+		if (userData[user].games[gameid]) {
 			numPlayers++;
 		}
 	}
 	return numPlayers;
 }
 
+/**
+ 
+ */
 exports.getUsersToNotify = function(room, time) {
 	var userData = room.getUserData();
-	var gameAgg = {};	
+	var gameAgg = {};
 
 	//Filter out to only get users with active timeline
 	//Also returns only the game data, since timeline is not needed.
 	var eligbleUsers = _und.chain(userData).
 	keys().
-	map(function(userName){
-		if(room.userHasActiveTimeline(userName, time)){
+	map(function(userName) {
+		if (room.userHasActiveTimeline(userName, time)) {
 			console.log(userData[userName].games);
-			return {name: userName, games: userData[userName].games}
+			return {
+				name: userName,
+				games: userData[userName].games
+			}
 		}
 	}).value();
 
@@ -72,54 +78,70 @@ exports.getUsersToNotify = function(room, time) {
 
 
 	//Group each player into what numplayer group for each game so game:min2(user1..n), game2 etc...
-	var numberOfPlayersInEachGroupPerGame  = _und.chain(allGames).
-	map(function(gameid){
+	var numberOfPlayersInEachGroupPerGame = _und.chain(allGames).
+	map(function(gameid) {
 		//This basically creates a group for each game and puth each user in them
 		var agg = _und.chain(eligbleUsers).
-		map(function(user){
+		map(function(user) {
 			var a = _und.chain(user.games).
-			filter(function(game){
+			filter(function(game) {
 				return gameid == game.id;
 			}).
-			first().//Only ever one element so we just fetch it
+			first(). //Only ever one element so we just fetch it
 			value();
-			return {name:user.name, data: a}
-		}).groupBy(function(user){//Group the users by category of minimum players so like [min1:[user1..n], .., minN[userk..P]]
+			return {
+				name: user.name,
+				data: a
+			}
+		}).groupBy(function(user) { //Group the users by category of minimum players so like [min1:[user1..n], .., minN[userk..P]]
 			return user.data.numPlayers;
-		}).map(function(x){
-			var users = _und.map(x,function(user){
+		}).map(function(x) {
+			var users = _und.map(x, function(user) {
 				return user.name;
 			});
-			return {numPlayers: x[0].data.numPlayers, users: users}	
+			return {
+				numPlayers: x[0].data.numPlayers,
+				users: users
+			}
 		}).value()
-		console.log("agg",  agg)
+		console.log("agg", agg)
 
-		return {gameid: gameid, agg: agg};
+		return {
+			gameid: gameid,
+			agg: agg
+		};
 	}).value()
 
 	//Find which players to notify
 	var playersToNotify = _und.chain(numberOfPlayersInEachGroupPerGame).
-	//Map over each game and return for each the elighble users
-	map(function(game){
+		//Map over each game and return for each the elighble users
+	map(function(game) {
 		return _und.chain(game.agg).
-		map(function(pair){
+		map(function(pair) {
 			console.log("lol", pair)
 			var users = _und.chain(game.agg).
-			filter(function(x){ //A group of users are eligble to play with anothher group if they have at most the same amount of min number of players
+			filter(function(x) { //A group of users are eligble to play with anothher group if they have at most the same amount of min number of players
 				return x.numPlayers <= pair.numPlayers
-			}).map(function(x){
+			}).map(function(x) {
 				return x.users
 			}).
 			value()
 
-			return {gameid: game.gameid, numPlayers: pair.numPlayers, eligble: users}
+			return {
+				gameid: game.gameid,
+				numPlayers: pair.numPlayers,
+				eligble: users
+			}
 		}).
-		filter(function(pair){
-			
+		filter(function(pair) {
+
 			return pair.numPlayers <= pair.eligble.length; //Filter out which users have a grouping that can match for a game
 		}).
-		map(function(pair){
-			return {gameid: pair.gameid, eligble: pair.eligble};
+		map(function(pair) {
+			return {
+				gameid: pair.gameid,
+				eligble: pair.eligble
+			};
 		}).
 		value();
 
@@ -131,13 +153,15 @@ exports.getUsersToNotify = function(room, time) {
 
 	console.log("playersToNotify", playersToNotify)
 
+	return playersToNotify;
+
 }
 
 /**
  * Returns a room object
  * @param roomId The id of the room
  * @returns {Room} Object representing all the data inside a room
-*/
+ */
 exports.getRoom = function(roomId) {
 	return rooms[roomId];
 }
@@ -145,49 +169,52 @@ exports.getRoom = function(roomId) {
 /**
  * Generates a random alphanumeric string of 8 characters
  * @returns {string} A random alphanumeric string
-*/
-exports.generateRoomId = function(){
+ */
+exports.generateRoomId = function() {
 	function randomString(length, chars) {
-	    var result = '';
-	    for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
-	    return result;
+		var result = '';
+		for (var i = length; i > 0; --i) result += chars[Math.round(Math.random() * (chars.length - 1))];
+		return result;
 	}
 	return randomString(8, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 }
 
 
-var Room = function () {
+var Room = function() {
 	var INITIAL_GUEST_NAME = 'Guest ';
 	var userdata = {};
 	var gamedata = {};
 
-	var getGamesForUser = function(userName){
+	var getGamesForUser = function(userName) {
 		var user = getUser(userName);
-		return _und.map(user.games, function(game){
-			return {gameid: game.id, numPlayers: game.numPlayers}
+		return _und.map(user.games, function(game) {
+			return {
+				gameid: game.id,
+				numPlayers: game.numPlayers
+			}
 		}).
 		value();
 
 	}
 
-	var getAllAddedGames = function(){
+	var getAllAddedGames = function() {
 		return _und.chain(userdata).
-		map(function(data){
+		map(function(data) {
 			return _und.chain(data.games).
-			map(function(game){
+			map(function(game) {
 				//Fulhack för att game.id ibland är sträng och ibland int, kan inte hitta varför, så gör såhär så länge.
-				if(!(typeof( game.id) == typeof(''))){
-					game.id = ''+game.id;
+				if (!(typeof(game.id) == typeof(''))) {
+					game.id = '' + game.id;
 				}
 				return game.id;
 			}).value()
 		}).
 		flatten().
-		sortBy(function(gameid){
+		sortBy(function(gameid) {
 			return gameid;
 		}).
 		uniq(true)
-		.value();
+			.value();
 	}
 
 
@@ -195,7 +222,7 @@ var Room = function () {
 		return userdata[userName];
 	}
 
-	var userHasActiveTimeline = function (userName, time){
+	var userHasActiveTimeline = function(userName, time) {
 		var user = getUser(userName);
 		return user.timeline[time];
 	}
@@ -205,12 +232,15 @@ var Room = function () {
 	 * @param name The name the user wants
 	 * @returns {boolean} True if user was created, false otherwise
 	 */
-	var claimName = function (name) {
-		if(validName(name)) {
-			userdata[name] = {"id": nextId(), games: {}, timeline: {}};
+	var claimName = function(name) {
+		if (validName(name)) {
+			userdata[name] = {
+				"id": nextId(),
+				games: {},
+				timeline: {}
+			};
 			return true;
-		}
-		else
+		} else
 			return false;
 	};
 
@@ -218,11 +248,10 @@ var Room = function () {
 	 * Checks if a name is valid
 	 * @returns {boolean} True if name is valid, false otherwise
 	 */
-	var validName = function (name){
+	var validName = function(name) {
 		if (!name || userdata[name] || name.length > exports.MAX_USERNAME_LENGTH) {
 			return false;
-		}
-		else {
+		} else {
 			return true;
 		}
 	};
@@ -236,13 +265,13 @@ var Room = function () {
 		var nextNum = 1;
 		var originalName = name;
 		if (name === INITIAL_GUEST_NAME)
-			name = name+nextNum;
+			name = name + nextNum;
 
 		while (!claimName(name)) {
 			name = originalName + nextNum;
 
 			nextNum += 1;
-			if(name.length > exports.MAX_USERNAME_LENGTH) // If name is too long we give him guest name instead
+			if (name.length > exports.MAX_USERNAME_LENGTH) // If name is too long we give him guest name instead
 				name = INITIAL_GUEST_NAME;
 		}
 		return name;
@@ -252,7 +281,7 @@ var Room = function () {
 	 * Find the lowest unused "guest" name and claim it
 	 * @returns {*} A guest name
 	 */
-	var getGuestName = function () {
+	var getGuestName = function() {
 		return getName(INITIAL_GUEST_NAME);
 	};
 
@@ -260,7 +289,7 @@ var Room = function () {
 	 * Get userdata
 	 * @returns {{}} The current userdata
 	 */
-	var getUserData = function () {
+	var getUserData = function() {
 		return userdata;
 	};
 
@@ -269,7 +298,7 @@ var Room = function () {
 	 *	Delete a user
 	 * @param name The name of the user to delete
 	 */
-	var freeName = function (name) {
+	var freeName = function(name) {
 		if (userdata[name]) {
 			delete userdata[name];
 		}
@@ -345,7 +374,7 @@ var Room = function () {
 	 */
 	var addGame = function(name, gameid, callback) {
 		if (!getGame(gameid)) {
-			addGameByIdAPI(gameid, function(){
+			addGameByIdAPI(gameid, function() {
 				addGameToUser(name, gameid);
 				callback();
 			});
@@ -355,7 +384,7 @@ var Room = function () {
 		}
 	};
 
-	var updateNumPlayers = function(name, data){
+	var updateNumPlayers = function(name, data) {
 		userdata[name] = data;
 	}
 
@@ -383,9 +412,9 @@ var Room = function () {
 	 */
 	var gameSearchAPI = function(searchQuery, callback) {
 
-		var url = "http://www.giantbomb.com/api/games/?api_key="+giantbomb.API_KEY+"&format=json&filter=name:"+searchQuery+",platforms:94&sort=original_release_date:desc&field_list=name,id,image&limit=7";
+		var url = "http://www.giantbomb.com/api/games/?api_key=" + giantbomb.API_KEY + "&format=json&filter=name:" + searchQuery + ",platforms:94&sort=original_release_date:desc&field_list=name,id,image&limit=7";
 		console.log(url)
-		request(url, function (error, response, data) {
+		request(url, function(error, response, data) {
 			if (!error && response.statusCode == 200) {
 				data = JSON.parse(data);
 				if (data.results.length == 0) {
@@ -394,7 +423,7 @@ var Room = function () {
 						data: []
 					});
 				} else {
-					for (var i=0; i < data.results.length; i++) {
+					for (var i = 0; i < data.results.length; i++) {
 						if (!data.results[i].image)
 							continue;
 
@@ -409,7 +438,7 @@ var Room = function () {
 					});
 				}
 			} else if (error) {
-				console.log("gameSearchAPI failed (query: "+searchQuery+")");
+				console.log("gameSearchAPI failed (query: " + searchQuery + ")");
 			}
 		});
 	};
@@ -419,9 +448,9 @@ var Room = function () {
 	 * @param gameid The id of the game
 	 */
 	var addGameByIdAPI = function(gameid, callback) {
-		var url = "http://www.giantbomb.com/api/game/"+gameid+"/?api_key="+giantbomb.API_KEY+"&format=json&field_list=id,name,image";
-		console.log("api requesting for id = "+gameid);
-		request(url, function (error, response, data) {
+		var url = "http://www.giantbomb.com/api/game/" + gameid + "/?api_key=" + giantbomb.API_KEY + "&format=json&field_list=id,name,image";
+		console.log("api requesting for id = " + gameid);
+		request(url, function(error, response, data) {
 			if (!error && response.statusCode == 200) {
 				data = JSON.parse(data);
 				gamedata[data.results.id] = {
@@ -430,7 +459,7 @@ var Room = function () {
 				};
 				callback();
 			} else if (error) {
-				console.log("addGameByIdAPI failed (gameid: "+gameid+")");
+				console.log("addGameByIdAPI failed (gameid: " + gameid + ")");
 			}
 
 		});
